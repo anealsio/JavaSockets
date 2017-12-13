ef858acb08c11e80c6f5cecf5a64904714744e21
Anisio Santos <anisio.dev@gmail.com>
2017-12-13 01:31:30 -0300

    Revert "Codigo comentado"
    This reverts commit ee8e87ba482ec65e01edf2563b16fcb4643b3477.

--- Servidor/src/servidor/Servidor.java
+++ Servidor/src/servidor/Servidor.java
@@ -36,85 +36,99 @@ public class Servidor{
             // Prepara para enviar as informações ao cliente
             DataOutputStream paraCliente = 
                 new DataOutputStream(socketConexao.getOutputStream());
-
-            // Lê o comando do cliente
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Le o comando do cliente
             fraseCliente = doCliente.readLine();
             // Verifica o comando que foi dado
-            if(fraseCliente.equals("INDEX")){  
-                // Verifica se é um diretório
+            if(fraseCliente.equals("INDEX")){                
                 if(diretorio.isDirectory() == false){
-                    // Verifica se o diretorio existe
-                    if(diretorio.exists() == false){    
-                        // Responde ao cliente que o diretório não existe
+                    if(diretorio.exists() == false){                        
                         paraCliente.writeBytes("O diretorio nao existe!\n");
-                        // Flag de fim de mensagem
                         paraCliente.writeBytes("-1\n");
                     }else{                        
-                        // Responde ao cliente que nao é um diretório
                         paraCliente.writeBytes("Esse nome nao corresponde a um diretorio.\n");
-                        // Flag de fim de mensagem
                         paraCliente.writeBytes("-1\n");;
                     }
                 }else{
-                    // Lista em arquivos todos os arquivos presentes em diretório
-                    arquivos = diretorio.list();
-                    // Manda mensagem inicial para cliente
+                    arquivos = diretorio.list();                    
+                    numeroLinhas = arquivos.length + 1;                   
+                    
                     paraCliente.writeBytes( "Arquivos no diretorio " + diretorio + ".\n" );
-                    // Manda lista de arquivos para o cliente
+                    System.out.println("Arquivos no diretorio " + diretorio + ".");
                     for(int i = 0; i < arquivos.length; i++)
                         paraCliente.writeBytes(" " + arquivos[i]+"\n");
-                    // Flag de fim de mensagem
                     paraCliente.writeBytes( "-1\n" );
                 }
-            // Verifica se o comando é GET <nome do arquivo>
             }else if(fraseCliente.charAt(0) == 'G' && fraseCliente.charAt(1) == 'E' && fraseCliente.charAt(2) == 'T'){
-                // Separa em um array que tera apenas dois espaços, o primeiro null e o segundo o nome recebido
                 String nomeArquivo[] = fraseCliente.split("GET ");
-                // Lista em arquivos todos os arquivos presentes em diretório
                 arquivos = diretorio.list();  
-                // Inicia variavel para verificar existencia de arquivo
+                for (int i =0;i<nomeArquivo.length;i++){
+                    System.out.println(i + nomeArquivo[i]);
+                }
+                
+                //*
                 int verificador = -1;
-                // Verifica se o nome recebido pertence a algum arquivo nesse diretorio
                 for ( int i = 0 ; i < arquivos.length ; i++ ){
                     if( arquivos[i].equals(nomeArquivo[1]) ){
                        verificador = i;
                        break;
                     }
                 }
-                // Caso o arquivo exista
-                if ( verificador != -1 ){                 
-                    // Muda o nome do diretorio para o com o arquivo solicitado
+                if ( verificador != -1){                    
                     nomeDiretorio += "\\" + nomeArquivo[1];
-                    // Prepara para ler arquivo
+                    System.out.println(nomeDiretorio);
                     BufferedReader lerArquivo = new BufferedReader(new InputStreamReader(new FileInputStream(nomeDiretorio), "ISO-8859-1"));
-                    // String que receberá o conteúdo do arquivo
                     String conteudo;
-                    // Lendo todo o arquivo
                     while(( conteudo = lerArquivo.readLine()) != null){                          
                         if (conteudo == null)
                             break;       
                         paraCliente.writeBytes(conteudo + "\n");
                     }
-                    // Flag de fim da mensagem
-                    paraCliente.writeBytes("-1\n");
-                    // Fecha a conexão
-                    socketConexao.shutdownInput();
-                    socketConexao.shutdownOutput();
-                    socketConexao.close();
+                    /*
+                    String teste;
+                    int count = 0;
+                    while(( teste = lerArquivo.readLine()) != null){
+                        //linhasArquivo[i] = lerArquivo.readLine();
+                        if (teste == null)
+                            break;
+                        count ++;
+                    }
                     
-                }
-                // Caso o arquivo n exista
-                else{                    
-                    // Responde ao cliente que o diretório não existe
-                    paraCliente.writeBytes("ERROR\n");
-                    // Flag de fim da mensagem
+                    String linhasArquivo[] = new String[count];
+                    for ( int i = 0 ; i < count ; i++ ){
+                        linhasArquivo[i] = lerArquivo.readLine();
+                    }
+                    count ++;
+                    paraCliente.writeBytes( count + "\n" );
+                    paraCliente.writeBytes( "OK\n" ); 
+                    for (int i = 0 ; i < linhasArquivo.length ; i++ ){
+                        paraCliente.writeBytes( linhasArquivo[i] + "\n" );
+                    }                     
+                    ///*/
                     paraCliente.writeBytes("-1\n");
-                    // Fecha a conexão
-                    socketConexao.shutdownInput();
-                    socketConexao.shutdownOutput();
-                    socketConexao.close();
-                }                
+                }else{                    
+                    paraCliente.writeBytes("ERROR\n");
+                    paraCliente.writeBytes("-1\n");
+                    //socketConexao.shutdownInput();
+                    //socketConexao.shutdownOutput();
+                   // socketConexao.close();
+                }
+                //*/
             }   
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /*
+            // Recebe a frase informada pelo cliente e passa para maiúscula
+            fraseCliente = doCliente.readLine();
+            fraseMaiuscula = fraseCliente.toUpperCase() + '\n';
+
+            // Imprime os valores recebido e a ser enviado
+            System.out.println("FRASE DO CLIENTE: " + fraseCliente);
+            System.out.println("FRASE DO SERVIDOR: " + fraseMaiuscula);
+
+            // Envia as informações ao cliente
+            paraCliente.writeBytes(fraseMaiuscula);
+            //*/
         }
     }
+
 }
